088f89344a114ee1c85eb3ed1336612c4fadc9b9
kyle65463 <kyle65463@gmail.com>
2023-04-01 23:54:29 +0800

    Updated key from serial to text

--- migrations/20230401221023_initial_schemas.sql
+++ migrations/20230401221023_initial_schemas.sql
@@ -1,23 +1,25 @@
 -- +goose Up
 -- +goose StatementBegin
 CREATE TABLE IF NOT EXISTS pages (
-    id SERIAL PRIMARY KEY,
+    key TEXT PRIMARY KEY,
     data BYTEA NOT NULL,
-    next_page_key INTEGER REFERENCES pages(id),
+    next_page_key TEXT REFERENCES pages(key),
     created_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
 CREATE INDEX IF NOT EXISTS pages_created_at ON pages(created_at);
 
 CREATE TABLE IF NOT EXISTS lists (
-    id SERIAL PRIMARY KEY,
-    next_page_key INTEGER REFERENCES pages(id),
+    key TEXT PRIMARY KEY,
+    next_page_key TEXT REFERENCES pages(key),
     created_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
--- +goose StatementEnd
 
+-- +goose StatementEnd
 -- +goose Down
 -- +goose StatementBegin
 DROP TABLE lists;
+
 DROP TABLE pages;
+
 -- +goose StatementEnd
