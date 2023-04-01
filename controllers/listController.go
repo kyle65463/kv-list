0d88058982c77785329082fe0981dcd24ce77fc9
kyle65463 <kyle65463@gmail.com>
2023-04-01 23:20:58 +0800

    Implemented GetListHead controller

--- controllers/listController.go
+++ controllers/listController.go
@@ -1,13 +1,47 @@
 package controllers
 
 import (
+	"context"
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/kyle65463/kv-list/database"
 )
 
+type ListHeadResponse struct {
+	ID          *int   `json:"id"`
+	NextPageKey *int   `json:"nextPageKey"`
+}
+
 func GetListHead(c *gin.Context) {
-	c.JSON(http.StatusNotImplemented, gin.H{
-		"message": "Get Head",
-	})
+	// Parse request
+	id := c.Param("id")
+
+	// Acquire db connection
+	conn, err := database.DbPool.Acquire(context.Background())
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": fmt.Sprintf("Failed to acquire connection"),
+		})
+		return
+	}
+	defer conn.Release()
+
+	// Read the list head from db
+	var page ListHeadResponse
+	err = conn.QueryRow(
+		context.Background(),
+		"SELECT id, next_page_key FROM lists WHERE id = $1",
+		id,
+	).Scan(&page.ID, &page.NextPageKey)
+	if err != nil {
+		// TODO: Handle no result error
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": fmt.Sprintf("Failed to query database: %v", err),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, page)
 }
