fd6c31d5e00b904626dec769896ad762d1a8a48e
kyle65463 <kyle65463@gmail.com>
2023-04-02 02:48:14 +0800

    Refactored pageController to use pgx v5

--- controllers/pageController.go
+++ controllers/pageController.go
@@ -5,47 +5,46 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/jackc/pgx/v4"
 	"github.com/kyle65463/kv-list/database"
 )
 
 type PageResponse struct {
-	Key         *string `json:"key"`
+	Key         string  `json:"key"`
 	Data        []byte  `json:"data"`
 	NextPageKey *string `json:"nextPageKey"`
 }
 
-func GetPage(c *gin.Context) {
-	// Parse request
-	key := c.Param("key")
+func GetPage(db database.PgxInterface) func(c *gin.Context) {
+	return func(c *gin.Context) {
+		// Parse request
+		key := c.Param("key")
 
-	// Acquire db connection
-	conn, err := database.DbPool.Acquire(context.Background())
-	if err != nil {
-		c.Status(http.StatusInternalServerError)
-		return
-	}
-	defer conn.Release()
-
-	// Read the page from db
-	var page PageResponse
-	err = conn.QueryRow(
-		context.Background(),
-		"SELECT key, data, next_page_key FROM pages WHERE key = $1",
-		key,
-	).Scan(&page.Key, &page.Data, &page.NextPageKey)
-	if err != nil {
-		if err == pgx.ErrNoRows {
-			// No result found
-			c.JSON(http.StatusBadRequest, gin.H{
-				"error": "No result found",
-			})
-		} else {
-			// Other errors
+		// Read the page from db
+		rows, err := db.Query(context.Background(),
+			"SELECT key, data, next_page_key FROM pages WHERE key = $1",
+			key,
+		)
+		if err != nil {
 			c.Status(http.StatusInternalServerError)
+			return
 		}
-		return
-	}
+		defer rows.Close()
 
-	c.JSON(http.StatusOK, page)
+		// Check if the result is empty
+		if !rows.Next() {
+			// No result found
+			c.JSON(http.StatusBadRequest, gin.H{"error": "No result found"})
+			return
+		}
+
+		// Parse the row
+		var page PageResponse
+		err = rows.Scan(&page.Key, &page.Data, &page.NextPageKey)
+		if err != nil {
+			c.Status(http.StatusInternalServerError)
+			return
+		}
+
+		c.JSON(http.StatusOK, page)
+	}
 }
