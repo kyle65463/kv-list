6e8488a267d1bad851d649e4a6f3d43054cea62a
kyle65463 <kyle65463@gmail.com>
2023-04-02 03:24:10 +0800

    Added TestShouldRespondBadRequestWhenNoPageFound

--- controllers/page_controller_test.go
+++ controllers/page_controller_test.go
@@ -46,3 +46,19 @@ func TestShouldGetPageWithNextPageKey(t *testing.T) {
 	assertResponseStatus(w, http.StatusOK, t)
 	assertResponseBody(w, data, t)
 }
+
+func TestShouldRespondBadRequestWhenNoPageFound(t *testing.T) {
+	// Setup the test
+	key := "test_key"
+	mock, c, w := setupTest(t, gin.Params{{Key: "key", Value: key}})
+
+	// Mock the empty query
+	rows := mock.NewRows([]string{"key", "data", "next_page_key"})
+	mock.ExpectQuery("SELECT key, data, next_page_key FROM pages").WithArgs(key).WillReturnRows(rows)
+
+	// Call the GetPage function
+	GetPage(mock)(c)
+
+	// Check the response
+	assertResponseStatus(w, http.StatusBadRequest, t)
+}
